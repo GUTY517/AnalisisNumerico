110f7166e30bdce7e6edf03eb477d24954946ad2
Mateo Guty <mateogutigomez@gmail.com>
2020-10-21 17:17:49 -0500

    Adding numpy jacobi method

--- project/python/jacobi.py
+++ project/python/jacobi.py
@@ -2,29 +2,26 @@
 """Solves the equation Ax=vector_b via the Jacobi iterative method."""
 
 from __future__ import print_function
-from pprint import pprint
-from numpy import array, zeros, diag, diagflat, dot
+import numpy as np
+# from scipy.linalg import solve   #Uncomment if using last lines
 
 def jacobi():
-    '''Jacobi implementation'''
-    matrix_a = array([[2.0, 1.0], [5.0, 7.0]])  # Change
-    vector_b = array([11.0, 13.0])            # Change
-    guess_vector = array([1.0, 1.0])          # Change
-    iterations = int(input("Number of iterations: "))
+    '''Jacobi method using numpy'''
+    matrix_a = np.array(
+        [[4.0, -2.0, 1.0], [1.0, -3.0, 2.0], [-1.0, 2.0, 6.0]])  # Change
+    vector_b = [1.0, 2.0, 3.0]                                   # Change
+    guess = [1.0, 1.0, 1.0]                                      # Change
+    iterations = int(input("Input iterations: "))
+    diagonal_matrix = np.diag(matrix_a)
+    hollow_matrix = matrix_a - np.diagflat(diagonal_matrix)
 
-    # Create an initial guess if needed
-    if guess_vector is None:
-        guess_vector = zeros(len(matrix_a[0]))
-
-    # Create a vector of the diagonal elements of matrix_a
-    # and subtract them from matrix_a
-    diagonal_matrix = diag(matrix_a)
-    hollow_matrix = matrix_a - diagflat(diagonal_matrix)
-
-    # Iterate for iterations times
     for _ in range(iterations):
-        guess_vector = (vector_b - dot(hollow_matrix, guess_vector)) / diagonal_matrix
-    print("Solved_matrix:\n")
-    return guess_vector
+        guess = (vector_b - np.dot(hollow_matrix, guess)) / diagonal_matrix
+    return guess
 
-pprint(jacobi())
+print("Answer: ", jacobi())
+
+# matrix_a = np.array([[4.0, -2.0, 1.0], [1.0, -3.0, 2.0], [-1.0, 2.0, 6.0]])  # Change
+# vector_b = [1.0, 2.0, 3.0]                                   # Change
+# guess = [1.0, 1.0, 1.0]                                      # Change
+# print("Answer: ", solve(matrix_a, vector_b))
