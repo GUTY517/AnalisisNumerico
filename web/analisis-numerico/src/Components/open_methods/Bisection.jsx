dd64243914997d0898560550170da2a40cff98f2
Mateo Marulanda <marmacesal@gmail.com>
2020-11-05 00:30:06 -0500

    Bisection: fetching data from flask backend

--- web/analisis-numerico/src/Components/open_methods/Bisection.jsx
+++ web/analisis-numerico/src/Components/open_methods/Bisection.jsx
@@ -1,11 +1,48 @@
 import React, { useState, useEffect } from "react";
 import Geogebra from "../assets/Geogebra";
 import json_data from "../../json_data/bisection.json";
+import { isCompositeComponent } from "react-dom/test-utils";
+import axios from "axios";
+
+
 const Bisection = () => {
   const [data, setData] = useState([]);
+  const [form_data, setFormData] = useState({
+    func: "",
+    initial_a: 0,
+    initial_b: 0,
+    tolerance: 1e-7,
+    iterations: 100,
+  });
+
   const calculate = (event) => {
     event.preventDefault();
     setData(json_data);
+  };
+
+  const handleInputChange = (e) => {
+
+    setFormData({
+      ...form_data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubtmit = async (e) => {
+    e.preventDefault();
+
+
+    const {func, initial_a, initial_b, tolerance, iterations} = form_data;
+    const body = {
+      function: func,
+      initial_a: parseFloat(initial_a),
+      initial_b: parseFloat(initial_b),
+      tolerance: parseFloat(tolerance),
+      iterations: parseFloat(iterations)
+    }
+    const result = await axios.post("http://127.0.0.1:5000/bisection", body);
+    setData(result.data);
+    showData()
   };
   const showData = () => {
     if (data.length > 0) {
@@ -18,26 +55,22 @@ const Bisection = () => {
           <table class="table">
             <thead>
               <tr>
-                {
-                  columns.map(column => (
+                {columns.map((column) => (
                   <th scope="col">{column}</th>
-                  ))
-                }
+                ))}
               </tr>
             </thead>
             <tbody>
-              {
-                results.map((result, index) => (
-                  <tr id={index}>
-                    <th>{result.Iteration}</th>
-                <th>{result.a}</th>
-                <th>{result.xm}</th>
-                    <th>{result.b}</th>
-                    <th>{result["f(xm)"]}</th>
-                    <th>{result.Error}</th>
-                  </tr>
-                ))
-              }
+              {results.map((result, index) => (
+                <tr id={index}>
+                  <th>{result.Iteration}</th>
+                  <th>{result.a}</th>
+                  <th>{result.xm}</th>
+                  <th>{result.b}</th>
+                  <th>{result["f(xm)"]}</th>
+                  <th>{result.Error}</th>
+                </tr>
+              ))}
             </tbody>
           </table>
         </React.Fragment>
@@ -52,12 +85,14 @@ const Bisection = () => {
       <div className="container mt-2">
         <div className="row">
           <div className="col-6 mt-3">
-            <form className="m-auto" onSubmit={calculate}>
+            <form className="m-auto" onSubmit={handleSubtmit}>
               <div className="form-group">
                 <input
                   type="text"
                   className="form-control"
-                  placeholder="Function"
+                  placeholder="f(x)="
+                  name="func"
+                  onChange={handleInputChange}
                 ></input>
               </div>
               <div className="form-group">
@@ -65,6 +100,8 @@ const Bisection = () => {
                   type="text"
                   className="form-control"
                   placeholder="Initial a"
+                  name="initial_a"
+                  onChange={handleInputChange}
                 ></input>
                 <small id="initialAHelp" class="form-text text-muted">
                   First element included in a range
@@ -75,6 +112,8 @@ const Bisection = () => {
                   type="text"
                   className="form-control"
                   placeholder="Initial b"
+                  name="initial_b"
+                  onChange={handleInputChange}
                 ></input>
                 <small id="initialAHelp" class="form-text text-muted">
                   Last element included in a range
@@ -85,6 +124,9 @@ const Bisection = () => {
                   type="text"
                   className="form-control"
                   placeholder="Tolerance"
+                  name="tolerance"
+                  value={form_data.tolerance}
+                  onChange={handleInputChange}
                 ></input>
               </div>
               <div className="form-group">
@@ -92,6 +134,9 @@ const Bisection = () => {
                   type="text"
                   className="form-control"
                   placeholder="Iterations"
+                  name="iterations"
+                  value={form_data.iterations}
+                  onChange={handleInputChange}
                 ></input>
               </div>
               <div className="form-group">
