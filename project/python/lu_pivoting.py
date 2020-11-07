755307ef370e92f6be1c06b9cc7bcf84aedda09c
Mateo Guty <mateogutigomez@gmail.com>
2020-11-07 15:36:18 -0500

    Fixing variables

--- project/python/lu_pivoting.py
+++ project/python/lu_pivoting.py
@@ -2,8 +2,9 @@
 '''LU Pivoting method implementation using scipy'''
 
 import numpy as np
-import scipy.linalg as la
+from scipy.linalg import lu
 from scipy.linalg import solve
+
 
 def matrix_input():
     '''Console matrix input'''
@@ -19,35 +20,37 @@ def matrix_input():
 def lu_pivoting():
     '''LU pivoting method'''
     matrix_a = np.array(matrix_input())
-    matrix_b = np.array(matrix_input())
+    vector = np.array(matrix_input())
 
-    augmented_matrix = np.append(matrix_a, matrix_b, axis=1)
+    augmented_matrix = np.append(matrix_a, vector, axis=1)
 
     print("Matrix A:")
     print(matrix_a)
     print()
-    print("Vector b:")
-    print(matrix_b)
+    print("Vector:")
+    print(vector)
     print()
     print("Augmented matrix:")
     print(augmented_matrix)
     print()
-    pivoted_matrix, triangular_matrix, solved_matrix = la.lu(matrix_a)
-    print("Pivoted Matrix (P):")
-    print(pivoted_matrix)
+    permuted_matrix, lower_triangular_matrix, upper_triangular_matrix = lu(
+        matrix_a)
+    print("Permuted matrix (P):")
+    print(permuted_matrix)
     print()
-    print("Triangular matrix (L):")
-    print(triangular_matrix)
+    print("Lower triangular matrix (L):")
+    print(lower_triangular_matrix)
     print()
-    print("Solved matrix (U):")
-    print(solved_matrix)
+    print("Upper triangular matrix (U):")
+    print(upper_triangular_matrix)
     print()
     print("Solved function using regression and progression:")
-    print(solve(matrix_a, matrix_b))
+    print(solve(matrix_a, vector))
+
 
 lu_pivoting()
 
 
 ####### Tests #######
 # matrix_a = np.array([[4, -1, 0, 3], [1, 15.5, 3, 8], [0, -1.3, -4, 1.1], [14, 5, -2, 30]])
-# matrix_b = np.array([[1], [1], [1], [1]])
+# vector = np.array([[1], [1], [1], [1]])
