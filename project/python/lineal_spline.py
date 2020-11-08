c9e9e3a91ab98e78390b2420df29b3bb6361a1ca
Mateo Guty <mateogutigomez@gmail.com>
2020-11-08 18:51:39 -0500

    Adding lineal spline method

---
+++ project/python/lineal_spline.py
@@ -0,0 +1,104 @@
+#! /usr/bin/env python3
+'''Linear spline implementation'''
+
+import numpy as np
+from numpy.linalg import det
+from scipy.linalg import lu
+
+
+def lineal_spline(data):
+    '''Linear spline method'''
+    data_size = 2 * (len(data) - 1) + 1
+    matrix = resulted_matrix(data, data_size)
+    gen = matrix
+    determinant = check_det(matrix)
+    if determinant[0] == 1:
+        return determinant
+    _, matrix = lu(matrix, permute_l=True)
+    coefficients = polyniomial_values(matrix.tolist(), len(matrix))
+    polynomials = clean_output(coefficients, data)
+    return (0, polynomials, gen)
+
+
+def resulted_matrix(data, data_size):
+    '''Matrix creation by organizing and evaluating inputed data'''
+    result = []
+    row = list(np.zeros(data_size))
+    row[0] = data[0][0]
+    row[1] = 1
+    row[data_size-1] = data[0][1]
+    result.append(row)
+    column = 0
+    for i in range(1, len(data)):
+        row = list(np.zeros(data_size))
+        row[i-1 + column] = data[i][0]
+        row[i + column] = 1
+        row[data_size-1] = data[i][1]
+        column += 1
+        result.append(row)
+    column = 0
+    for j in range(1, len(data) - 1):
+        row = list(np.zeros(data_size))
+        row[column] = data[j][0]
+        row[column + 1] = 1
+        row[column + 2] = -data[j][0]
+        row[column + 3] = -1
+        column += 2
+        result.append(row)
+    return result
+
+
+def polyniomial_values(matrix, data_size):
+    '''Get polynomial values'''
+    vector = [0] * data_size
+    vector[data_size-1] = matrix[data_size-1][data_size] / \
+        matrix[data_size-1][data_size-1]
+    i = data_size-2
+    while i >= 0:
+        result = 0
+        counter = len(vector)-1
+        while counter >= 0:
+            result += (matrix[i][counter]*vector[counter])
+            counter -= 1
+        vector[i] = (matrix[i][data_size]-result)/matrix[i][i]
+        i -= 1
+    return vector
+
+
+def clean_output(coefficients, data):
+    '''Function to clean and organize the output'''
+    results = []
+    polynomials = []
+    cont = 0
+    for i in range(int(len(coefficients)/2)):
+        results.append([i, coefficients[cont], coefficients[cont+1]])
+        cont += 2
+
+    polynomials.append(["Polynomials", "Ranges"])
+    data_size = len(results)
+    for i in range(data_size):
+        polynom = f'P{i+1}='
+        polynom += str(results[i][1]) + "x "
+        if results[i][2] < 0:
+            polynom += str(results[i][2])
+        else:
+            polynom += "+" + str(results[i][2])
+        ranges = str(data[i][0]) + "<= x <= " + str(data[i+1][0])
+        polynomials.append([polynom, ranges])
+    return polynomials
+
+def check_det(data):
+    '''Get matrix determinant and check if is zero'''
+    data_size = len(data[0]) - 1
+    square_a = [x[0:data_size] for x in data]
+    if det(square_a) == 0:
+        return (
+            1, 'The generated matrix is not invertible. '
+            'You may want to select a different set of points')
+    return(0, "Ok.")
+
+X = [[0, 6], [1, 1], [2, 1], [3, 0]]
+res = lineal_spline(X)
+print(res[1])
+print()
+print(res[2])
