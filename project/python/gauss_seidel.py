b9e867f5a3e002382a8c867c3b2fa15d6668b913
Mateo Guty <mateogutigomez@gmail.com>
2020-11-09 19:38:40 -0500

    Adding Gauss Seidel method

--- project/python/gauss_seidel.py
+++ project/python/gauss_seidel.py
@@ -1,28 +1,99 @@
 #! /usr/bin/env python3
-'''Gauss seidel method using numpy'''
+'''Gauss Seidel implementation using norm 2'''
 
-from __future__ import print_function
+from math import sqrt
+from copy import copy
 import numpy as np
-# from scipy.linalg import solve   #Uncomment if using last lines
+from prettytable import PrettyTable
+from numpy.linalg import det, inv, LinAlgError
 
-def gauss_seidel():
-    '''Gauss seidel method'''
-    matrix_a = np.array([[4.0, -2.0, 1.0], [1.0, -3.0, 2.0], [-1.0, 2.0, 6.0]])     #Change
-    vector_b = [1.0, 2.0, 3.0]                                                      #Change
-    guess = [1, 1, 1]                                                               #Change
+
+def gauss_seidel(x_0, matrix, vector):
+    '''Gauss Seidel method'''
+    answers = []
+    new_x0 = copy(x_0)
+    data_size = len(matrix)
+    for i in range(data_size):
+        suma = 0
+        for j in range(data_size):
+            if j != i:
+                suma += matrix[i][j]*new_x0[j]
+        suma = (vector[i] - suma)/matrix[i][i]
+        new_x0[i] = suma
+        answers.append(suma)
+    return answers
+
+
+def norm_2(x_0, x_1):
+    '''Returns Gauss Seidel method using norm 2'''
+    answer = 0
+    data_size = len(x_0)
+    for i in range(data_size):
+        answer += ((x_1[i] - x_0[i])**2)
+    try:
+        if sqrt(answer):
+            error = sqrt(answer)
+            return error, 0
+    except OverflowError:
+        return 0, 1
+
+
+def matrix_input():
+    '''Console matrix_a input'''
+    rows = int(input("Enter number of rows: "))
+    columns = int(input("Enter number of columns: "))
+    print("Enter the entries in a single line (separated by space): ")
+    entries = list(map(float, input().split()))
+    matrix = np.array(entries).reshape(rows, columns)
+
+    return matrix
+
+
+def main():
+    '''Input variables and method execution'''
+    tolerance = float(input("Input tolerance: "))
     iterations = int(input("Input iterations: "))
-    lower_triangle = np.tril(matrix_a)
-    upper_triangle = matrix_a - lower_triangle
+    matrix = matrix_input()
+    vector = matrix_input()
+    x_0 = matrix_input()
 
-    for _ in range(iterations):
-        guess = np.dot(np.linalg.inv(lower_triangle), vector_b - np.dot(upper_triangle, guess))
-        # print("Iteration: ", i)                     #Uncomment if want to see the whole process
-        # print(guess)                                #Uncomment if want to see the whole process
-    return guess
+    determinant = det(matrix)
+    if determinant == 0:
+        return(1, "Determinant is ZERO (no unique solution)")
 
-print("Answer: ", gauss_seidel())
+    try:
+        inv(matrix)
+    except LinAlgError:
+        return(1, "The matrix is not invertible")
+
+    title = ['Iterations']
+    table_tittles = 0
+    iteration = 0
+    while table_tittles < len(x_0):
+        title.append(f"x{str(table_tittles)}")
+        table_tittles += 1
+
+    title.append("Error")
+    table = PrettyTable(title)
+    table.add_row([iteration] + x_0 + ["-"])
+    error = tolerance + 1
+    gauss_seidel_answer = gauss_seidel(x_0, matrix, vector)
+    while error > tolerance and iteration < iterations:
+        answers = gauss_seidel(x_0, matrix, vector)
+        error = norm_2(x_0, answers)[0]
+        iteration += 1
+        table.add_row([iteration] + answers + [error])
+        x_0 = copy(answers)
+    print(gauss_seidel_answer)
+    return table
 
 
-# matrix_a = np.array([[4.0, -2.0, 1.0], [1.0, -3.0, 2.0], [-1.0, 2.0, 6.0]])     #Change
-# vector_b = [1.0, 2.0, 3.0]                                                      #Change
-# print("Answer: ", solve(matrix_a, vector_b))  #Solution using scipy
+print(main())
+
+### Tests ###
+# tolerance = 1e-7
+# x_0 = [0, 0, 0, 0]
+# iterations = 100
+# matrix = [[4, -1, 0, 3], [1, 15.5, 3, 8], [0, -1.3, -4, 1.1], [14, 5, -2, 30]]
+# vector = [1, 1, 1, 1]
+# print(main())
