32f783ce9dfa5afc6fcc0f00410c25f6d51d6c87
Mateo Guty <mateogutigomez@gmail.com>
2020-11-09 19:53:58 -0500

    Fixing errors

--- project/python/jacobi.py
+++ project/python/jacobi.py
@@ -2,34 +2,35 @@
 '''New Jacobi method implementation'''
 
 from math import sqrt
+from copy import copy
 import numpy as np
 from prettytable import PrettyTable
 from numpy.linalg import inv, det, LinAlgError
 
 def matrix_input():
-    '''Console matrix_a input'''
+    '''Console matrix input'''
     rows = int(input("Enter number of rows: "))
     columns = int(input("Enter number of columns: "))
     print("Enter the entries in a single line (separated by space): ")
     entries = list(map(float, input().split()))
-    matrix_a = np.array(entries).reshape(rows, columns)
+    matrix = np.array(entries).reshape(rows, columns)
 
-    return matrix_a
+    return matrix
 
-def jacobi(x_0, matrix_a, vector):
+def jacobi(x_0, matrix, vector):
     '''Returns Jacobi answers'''
     answer = []
-    data_size = len(matrix_a)
+    data_size = len(matrix)
     for i in range(data_size):
         results = 0
         for j in range(data_size):
             if j != i:
-                results += matrix_a[i][j]*x_0[j]
-        results = (vector[i] - results)/matrix_a[i][i]
+                results += matrix[i][j]*x_0[j]
+        results = (vector[i] - results)/matrix[i][i]
         answer.append(results)
     return answer
 
-def new_norm(x_0, x_1):
+def norm_2(x_0, x_1):
     '''Returns vectors norm'''
     results = 0
     data_size = len(x_0)
@@ -46,19 +47,19 @@ def main():
     '''Returns Jacobi answer and calculation table'''
     tolerance = float(input("Input tolerance: "))
     iterations = int(input("Input iterations: "))
-    matrix_a = matrix_input()
+    matrix = matrix_input()
     vector = matrix_input()
     x_0 = matrix_input()
 
-    determinant = det(matrix_a)
+    determinant = det(matrix)
     if determinant == 0:
         return(1, "Determinant is zero")
     try:
-        inv(matrix_a)
+        inv(matrix)
     except LinAlgError:
         return 1, "Matrix is not invertible"
 
-    title = ['n']
+    title = ['iterations']
     answer = 0
     iterator = 0
     while answer < len(x_0):
@@ -68,28 +69,28 @@ def main():
     table = PrettyTable(title)
     table.add_row([iterator] + x_0 + ["-"])
     error = tolerance + 1
-    jacobi_response = jacobi(x_0, matrix_a, vector)
+    jacobi_response = jacobi(x_0, matrix, vector)
     while error > tolerance and iterator < iterations:
-        x_1 = jacobi(x_0, matrix_a, vector)
-        answer = new_norm(x_0,x_1)
+        x_1 = jacobi(x_0, matrix, vector)
+        answer = norm_2(x_0, x_1)
         error = answer[0]
         err = answer[1]
         if err == 1:
             return 1, "Result is too big"
         iterator += 1
         table.add_row([iterator] + x_1 + [error])
+        x_0 = copy(x_1)
+
     print("Answer (C) = ", jacobi_response)
     print()
-    return table
+    print(table)
 
-print(main())
+main()
 
 ### Tests ###
-# matrix_a = np.array([[4, -1, 0, 3], [1, 15.5, 3, 8], [0, -1.3, -4, 1.1], [14, 5, -2, 30]])
-# vector = np.array([[1], [1], [1], [1]])
 # tolerance = 1e-7
 # x_0 = [0,0,0,0]
 # iterations = 100
-# matrix_a = [[4, -1, 0, 3], [1, 15.5, 3, 8], [0, -1.3, -4, 1.1], [14, 5, -2, 30]]
+# matrix = [[4, -1, 0, 3], [1, 15.5, 3, 8], [0, -1.3, -4, 1.1], [14, 5, -2, 30]]
 # vector = [1,1,1,1]
-# print(main(tolerance,x_0,iterations,matrix_a,vector))
+# print(main(tolerance,x_0,iterations,matrix,vector))
