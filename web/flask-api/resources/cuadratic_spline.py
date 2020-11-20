ba6fc55af5dac57e0c104cccb23fd85f2fb676b2
Mateo Guty <mateogutigomez@gmail.com>
2020-11-20 11:59:45 -0500

    Fixing cuadratic spline web method

--- web/flask-api/resources/cuadratic_spline.py
+++ web/flask-api/resources/cuadratic_spline.py
@@ -124,7 +124,7 @@ def check_det(matrix):
     return(0, "OK")
 
 
-class cuadratic_spline(Resources):
+class cuadratic_spline(Resource):
 
     def post(self):
         body_params = request.get_json()
